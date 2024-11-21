e4e87f6c60d5d853187016acf7d9290455e9461c
Daniela Danada <dani.danadinha321@gmail.com>
2024-11-21 08:54:16 -0300

    Atualizando banco de dados

--- BD/confeitaria.sql
+++ BD/confeitaria.sql
@@ -3,14 +3,17 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17/11/2024 às 01:39
+-- Tempo de geração: 21/11/2024 às 12:47
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
+
+create database confeitaria;
+
+use confeitaria;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,7 +45,9 @@ CREATE TABLE `funcionarios` (
 --
 
 INSERT INTO `funcionarios` (`pk_funcionario`, `nome`, `cpf`, `telefone`, `endereco`, `senha`, `fk_funcao`) VALUES
-(1, 'zeca', NULL, NULL, NULL, 'tilapia123', NULL);
+(1, 'zeca', NULL, NULL, NULL, 'tilapia123', NULL),
+(2, 'lincoln', NULL, NULL, NULL, 'coxinha123', NULL),
+(3, 'Ana', '10101010', '19191919', 'Presidente Prudente', 'coxinha123', 2);
 
 -- --------------------------------------------------------
 
@@ -55,6 +60,14 @@ CREATE TABLE `funcoes` (
   `funcao` varchar(20) DEFAULT NULL,
   `salario` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `funcoes`
+--
+
+INSERT INTO `funcoes` (`pk_funcao`, `funcao`, `salario`) VALUES
+(1, 'Gerente', 10000),
+(2, 'Atendente', 2000);
 
 -- --------------------------------------------------------
 
@@ -69,6 +82,14 @@ CREATE TABLE `ingredientes` (
   `qtd_ingredientes` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Despejando dados para a tabela `ingredientes`
+--
+
+INSERT INTO `ingredientes` (`id_ingrediente`, `nome_ingrediente`, `data_validade_ingre`, `qtd_ingredientes`) VALUES
+(1, 'maçã', '2003-12-07', 20),
+(2, 'maçã', '1011-10-10', 10);
+
 -- --------------------------------------------------------
 
 --
@@ -81,8 +102,16 @@ CREATE TABLE `produto` (
   `data_fabricacao` date DEFAULT NULL,
   `data_validade` date DEFAULT NULL,
   `descricao_produto` varchar(250) NOT NULL,
-  `qtd_produto` int(11) DEFAULT NULL
+  `qtd_produto` int(11) DEFAULT NULL,
+  `preco` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `produto`
+--
+
+INSERT INTO `produto` (`id_produto`, `nome_produto`, `data_fabricacao`, `data_validade`, `descricao_produto`, `qtd_produto`, `preco`) VALUES
+(1, 'Banana', '1111-11-11', '1010-10-10', 'aaaaaaaaaaaa', 1, 1010);
 
 -- --------------------------------------------------------
 
@@ -134,25 +163,25 @@ ALTER TABLE `produto`
 -- AUTO_INCREMENT de tabela `funcionarios`
 --
 ALTER TABLE `funcionarios`
-  MODIFY `pk_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pk_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `funcoes`
 --
 ALTER TABLE `funcoes`
-  MODIFY `pk_funcao` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pk_funcao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `ingredientes`
 --
 ALTER TABLE `ingredientes`
-  MODIFY `id_ingrediente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ingrediente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restrições para tabelas despejadas
